b4e2b1f8926156ffc46e17955926dca1d104563e
Jakob Misbach <Jakob Misbach@DESKTOP-J9GA7IG>
2016-08-09 21:24:33 -0400

    Worked on game class

--- Tile Game/src/com/winfirst/main/Launcher.java
+++ Tile Game/src/com/winfirst/main/Launcher.java
@@ -1,9 +1,8 @@
 package com.winfirst.main;
 
-import com.winfirst.display.Display;
-
 public class Launcher {
 	public static void main(String[] args){
-		new Game("Game", 400, 400);
+		Game game = new Game("Game", 400, 400);
+		game.start();
 	}
 }
